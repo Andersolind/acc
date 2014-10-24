ad6163feed8a05fc71b6a338946316a4f564d789
jchen1175 <jchen.acgt@gmail.com>
2014-10-24 10:00:17 -0400

    fix personalized projects

---
+++ MBBVL/Controllers/PersonalizedProjectsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBBVL.Controllers
+{
+    public class PersonalizedProjectsController : Controller
+    {
+        //
+        // GET: /PersonalizedProjects/
+        // test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PersonalizedCustomProjects()
+        {
+            return View();
+        }
+        public ActionResult SiteDirectedMutagenesis()
+        {
+            return View();
+
+        }
+
+	}
+}

1c7a4d0cb5037580d817ddf047081b5e7588e0bc
Andersolind <andersolind@gmail.com>
2014-11-28 14:08:36 -0500

    layout changes

---
+++ MBBVL/Controllers/InvestorsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBBVL.Controllers
+{
+    public class InvestorsController : Controller
+    {
+        //
+        // GET: /Investors/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}

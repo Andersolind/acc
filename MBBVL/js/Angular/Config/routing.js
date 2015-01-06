cd695bf9c186334e1940c4007abfebda340b842d
Andersolind <andersolind@gmail.com>
2015-01-05 23:02:09 -0500

    layout changes

---
+++ MBBVL/js/Angular/Config/routing.js
@@ -0,0 +1,15 @@
+﻿app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/OrderForm', {
+            templateUrl: 'views/home/orderform.cshtml',
+            controller: 'js/Order-ctrl'
+        }).
+        when('/showOrders', {
+            templateUrl: 'templates/show-orders.html',
+            controller: 'ShowOrdersController'
+        }).
+        otherwise({
+            redirectTo: '/Order'
+        });
+  }]);

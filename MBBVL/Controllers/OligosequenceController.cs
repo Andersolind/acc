1fc50b35546ebf6cd9cd52b3498a88337c007250
Andersolind <andersolind@gmail.com>
2016-01-10 23:14:29 -0500

    Css changes

--- MBBVL/Controllers/OligosequenceController.cs
+++ MBBVL/Controllers/OligosequenceController.cs
@@ -1,16 +1,11 @@
 ﻿using MBBVL.Core;
-using MBBVL.HelperClass;
 using MBBVL.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Validation;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using System.Web.Http.Description;
-namespace MBBVL.Controllers {
+namespace MBBVL.Controllers
+{
     public class OligosequenceController : ApiController {
 
         public ApplicationDbContext db = new ApplicationDbContext();
@@ -94,23 +89,8 @@ namespace MBBVL.Controllers {
                 return Ok();
             } catch (Exception ex) {
                 return Ok(ex.InnerException.ToString());
-                // Retrieve the error messages as a list of strings.
-                //var errorMessages = ex.EntityValidationErrors
-                //    .SelectMany(x => x.ValidationErrors)
-                //    .Select(x => x.ErrorMessage);
-
-                //// Join the list to a single string.
-                //var fullErrorMessage = string.Join("; ", errorMessages);
-
-                //// Combine the original exception message with the new one.
-                //var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
-
-                //// Throw a new DbEntityValidationException with the improved exception message.
-                //throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+               
             }
-
-
-
         }
         // PUT api/<controller>/5
         public void Put(int id, [FromBody]string value) {

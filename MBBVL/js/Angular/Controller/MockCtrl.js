4e71a1c8ec3902bfee37a385565a504c17925b29
Andersolind <andersolind@gmail.com>
2016-02-19 00:31:09 -0500

    Need to fix model for presentation
    ..

--- MBBVL/js/Angular/Controller/MockCtrl.js
+++ MBBVL/js/Angular/Controller/MockCtrl.js
@@ -1,13 +1,25 @@
-﻿app.controller("MockCtrl", [
-    '$scope','$rootScope', '$http', function($scope,$rootScope, $http, GenericHelpers, ACGTFactory) {
+﻿app.controller("MockCtrl", ['$scope', '$http', 'GenericHelpers', 'ACGTFactory', '$rootScope', function ($scope, $http, GenericHelpers, ACGTFactory, $rootScope) {
 
+    //
+    var vm = this;
+    setupInitialRows();
+    //Create a new row
+    vm.AddOligonucleotideRow = function () {
+        //Push a new row
+        vm.OligonucleotideRow.push(GenericHelpers.oligonucleotideRow(vm.synthesisScale1Values, vm.purification, $scope.finalDeliveryForm, $scope.gmp3));
+    };
+
+    function setupInitialRows() {
+        // we initializ all our values here
+        vm.modificationList = GenericHelpers.modifications();
+        vm.gmp3 = [{ name: 'yes', value: 'yes' }, { name: 'no', value: 'no' }];
+        vm.synthesisScale1Values = [{ name: '0.02', value: '0.02' }, { name: '0.04', value: '0.04' }, { name: '0.2', value: '0.2' }, { name: '1', value: '1' }];
         //
-        var vm = this;
-      
-        vm.turnBillingOff = function () {
-            //Set all the form values to not enabled
-            $rootScope.dnaForm.txtFirstName.$setValidity('required', false);
-           
-        }
-        //
-    }]);
+        vm.purification = [{ name: 'Desalted', value: 'Desalted' }, { name: 'Rp-Cartidge', value: 'Rp-Cartidge' }];
+
+        vm.finalDeliveryForm = [{ name: 'Liquid', value: 'Liquid' }, { name: 'Dry', value: 'Dry' }];
+        vm.OligonucleotideRow = [{ OglioNumber: "", Qty: "", OligonucleotideSequence: '', SynthesisScale1: vm.synthesisScale1Values, SynthesisScaleValue: "", Modification: "", ModificationValue: "", FinalDeliveryForm: vm.finalDeliveryForm, FinalDeliveryFormValue: "", Purification: vm.purification, PurificationValue: "", GMP3: vm.gmp3, GmpValue: "", Price: "" }];
+
+    //    vm.OligonucleotideRow = [{ PrimerName: "", Qty: "", OligonucleotideSequence: '', SynthesisScale1: $scope.synthesisScale1Values, SynthesisScaleValue: "", Modification: "", ModificationValue: "", FinalDeliveryForm: $scope.finalDeliveryForm, FinalDeliveryFormValue: "", Purification: $scope.purification, PurificationValue: "", GMP3: $scope.gmp3, GmpValue: "", Price: "" }];
+    }
+}]);

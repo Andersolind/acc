cdc4a72a7bf9a1cc3ee4ecc7127b723beb5ba824
Andersolind <andersolind@gmail.com>
2016-02-29 19:41:50 -0500

    Updates
    Updates

--- MBBVL/js/Angular/Controller/MockCtrl.js
+++ MBBVL/js/Angular/Controller/MockCtrl.js
@@ -2,22 +2,48 @@
 
     //
     var vm = this;
-    var i = 1;
-    vm.oligoNumber = i ++;
+    vm.isBilling = false;
     setupInitialRows();
     //Create a new row
-    vm.OligonucleotideRow = function () {
+    vm.AddOligonucleotideRow = function () {
         //Push a new row
-        vm.oligoNumber = i++;
-        vm.NewOligonucleotideRow.push(GenericHelpers.newOligonucleotideRow(vm.oligoNumber,vm.fiveModifications, vm.threeModifications, vm.synthesisScale1Values, vm.finalDeliveryForm, vm.purification));
+        vm.OligonucleotideRow.push(GenericHelpers.oligonucleotideRow(vm.synthesisScale1Values, vm.purification, $scope.finalDeliveryForm, $scope.gmp3));
     };
 
-    vm.removeRow = function (idx) {
-        vm.oligoNumber = i--;
-        vm.NewOligonucleotideRow.splice(idx, 1);
-    };
+    vm.isBillingTheSame = function (value) {
+
+        if (value) {
+
+            vm.WrapperModel.Shipping.FirstName = vm.WrapperModel.Billing.FirstName;
+            vm.WrapperModel.Shipping.LastName = vm.WrapperModel.Billing.LastName;
+            vm.WrapperModel.Shipping.Institution = vm.WrapperModel.Billing.Institution;
+            vm.WrapperModel.Shipping.Country = vm.WrapperModel.Billing.Country[2];
+            vm.WrapperModel.Shipping.ShippingAddress = vm.WrapperModel.Billing.BillingAddress;
+            vm.WrapperModel.Shipping.PostalCode = vm.WrapperModel.Billing.PostalCode;
+            vm.WrapperModel.Shipping.Phone = vm.WrapperModel.Billing.BillingPhone;
+            vm.WrapperModel.Shipping.Extention = vm.WrapperModel.Billing.Extention;
+            vm.WrapperModel.Shipping.Email = vm.WrapperModel.Billing.Email;
+        }
+    }
+
+    vm.changedValuesBilling = function (value) {
+        vm.WrapperModel.Billing.Country = value;
+    }
+    vm.changedValuesShipping = function (value) {
+        vm.WrapperModel.Shipping.Country = value;
+    }
+
 
     function setupInitialRows() {
+        //Create a blank object for binding later
+        vm.WrapperModel = {
+            Billing: {},
+            Shipping: {}
+            //  Oligosequence: $scope.OligonucleotideRow
+        };
+        //  vm.WrapperModel.Shipping.code = null;
+        //  Oligosequence: $scope.OligonucleotideRow
+
         // we initializ all our values here
         vm.modificationList = GenericHelpers.modifications();
         vm.gmp3 = [{ name: 'yes', value: 'yes' }, { name: 'no', value: 'no' }];
@@ -28,13 +54,14 @@
         vm.internalModification = GenericHelpers.internalModification();
         //Three Modifications 
         vm.threeModifications = GenericHelpers.threeModifications();
-        //Increment a number 
-       
         vm.purification = [{ name: 'Desalted', value: 'Desalted' }, { name: 'Cartridge', value: 'Cartridge' }, { name: 'HPLC', value: 'HPLC' }, { name: 'PAGE', value: 'PAGE' }];
 
         vm.finalDeliveryForm = [{ name: 'Liquid', value: 'Liquid-H2O' }, { name: 'Dry', value: 'Dry-Lyophilised' }];
-        vm.NewOligonucleotideRow = [{ OglioNumber: vm.oligoNumber, Qty: "", OligonucleotideSequence: '', Five5Modifications: vm.fiveModifications, InternalModification: vm.threeModifications, ThreeModifications: vm.threeModifications, SynthesisScale1: vm.synthesisScale1Values, SynthesisScaleValue: "", Modification: "", ModificationValue: "", FinalDeliveryForm: vm.finalDeliveryForm, FinalDeliveryFormValue: "", Purification: vm.purification, PurificationValue: "", GMP3: vm.gmp3, GmpValue: "", Price: "" }];
-
-    //    vm.OligonucleotideRow = [{ PrimerName: "", Qty: "", OligonucleotideSequence: '', SynthesisScale1: $scope.synthesisScale1Values, SynthesisScaleValue: "", Modification: "", ModificationValue: "", FinalDeliveryForm: $scope.finalDeliveryForm, FinalDeliveryFormValue: "", Purification: $scope.purification, PurificationValue: "", GMP3: $scope.gmp3, GmpValue: "", Price: "" }];
+        vm.OligonucleotideRow = [{ OglioNumber: "", Qty: "", OligonucleotideSequence: '', Five5Modifications: vm.fiveModifications, InternalModification: vm.threeModifications, ThreeModifications: vm.threeModifications, SynthesisScale1: vm.synthesisScale1Values, SynthesisScaleValue: "", Modification: "", ModificationValue: "", FinalDeliveryForm: vm.finalDeliveryForm, FinalDeliveryFormValue: "", Purification: vm.purification, PurificationValue: "", GMP3: vm.gmp3, GmpValue: "", Price: "" }];
+        vm.countriesList = GenericHelpers.country_list();
+        //    vm.OligonucleotideRow = [{ PrimerName: "", Qty: "", OligonucleotideSequence: '', SynthesisScale1: $scope.synthesisScale1Values, SynthesisScaleValue: "", Modification: "", ModificationValue: "", FinalDeliveryForm: $scope.finalDeliveryForm, FinalDeliveryFormValue: "", Purification: $scope.purification, PurificationValue: "", GMP3: $scope.gmp3, GmpValue: "", Price: "" }];
     }
+
+
+
 }]);
